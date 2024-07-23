b6ec83c277678a78317e01d1997649650ba7ea22
nick-name34 <nikkunemu34@gmail.com>
2024-07-23 23:54:19 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## 概要
+
+特定のWebページを定期的に監視し、ページが更新されていたらLINEに通知するシステム
+
+## セットアップ
+
+### LINE Notifyの登録
+
+「手順」
+
+1. 自身のLINEで通知を受けたいトークルームを作成する
+2. [https://notify-bot.line.me/ja/](https://notify-bot.line.me/ja/)にアクセス
+3. ［TODO］
+
+### インストール
+
+```
+pip install beautifulsoup4
+```
+
+```
+pip install requests
+```
+
+```
+pip install python-dotenv
+```
---
+++ src/config.py
@@ -0,0 +1,16 @@
+# config.py
+
+import os
+from dotenv import load_dotenv
+
+# .env ファイルの内容を読み込む
+load_dotenv()
+
+# 設定情報を辞書として管理
+CONFIG = {
+    'URL': 'https://www.sample',  # 監視対象のURL。現在はサンプルのURLを設定している
+    'HASH_FILE': 'page_hash.txt',  # ハッシュ値を保存するファイル
+    'LINE_NOTIFY_API': 'https://notify-api.line.me/api/notify',  # LINE NotifyのAPIエンドポイント
+    'LINE_TOKEN': os.getenv('LINE_NOTIFY_TOKEN_DEV'),  # LINE Notifyのトークン
+    'USER_AGENT' : os.getenv('USER_AGENT')
+}
